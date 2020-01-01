c882062794a5ea3094f34a274a2954c03c84a7b3
Oscar-Laptop <oskrr67@gmail.com>
2020-01-01 12:34:48 -0600

    Ingreso de primeras carpetas al repositorio

--- EDD Learning/src/edd/learning/EDDLearning.java
+++ EDD Learning/src/edd/learning/EDDLearning.java
@@ -1,13 +1,19 @@
 package edd.learning;
+
+import ventanas.Login;
+
 /**
  *
  * @author Oscar-Laptop
  */
 public class EDDLearning {
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Login inicio = new Login();
+        inicio.setVisible(true);
     }
 }
