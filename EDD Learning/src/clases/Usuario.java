f620bde62a61043e2330bee0e6ca981659a52f9a
Oscar-Laptop <oskrr67@gmail.com>
2020-01-02 17:18:11 -0600

    Inicio de cracion de Tabla Hash g

---
+++ EDD Learning/src/clases/Usuario.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+/**
+ *
+ * @author Oscar-Laptop
+ */
+public class Usuario {
+    
+    private String nombre;
+    private String apellido;
+    private String carne;
+    private String password;
+
+    public Usuario(String name, String lsname, String carne, String pass) {
+        this.nombre=name;
+        this.apellido=lsname;
+        this.carne=carne;
+        this.password=pass;
+    }
+    
+    void setNombre(String name){this.nombre=name;}
+    void setApellido(String A){this.apellido=A;}
+    void setCarne(String C){this.carne=C;}
+    void setPassword(String P){this.password=P;}
+    
+    String getNombre(){return this.nombre;}
+    String getApellido(){return this.apellido;}
+    String getCarne(){return this.carne;}
+    String getPassword(){return this.password;}
+       
+
+}
